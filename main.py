1bf37a204c39d9da9680c69ed3dad38d5d3d15d6
Shubham-1-cell <62086183+Shubham-1-cell@users.noreply.github.com>
2021-07-17 19:43:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 
-for i in range(30):
+for i in range(30, 50):
     d = str(i) + 'days ago'
     with open('bot.txt', 'a') as file:
         file.write(d + '\n')
     os.system('git add bot.txt')
     os.system('git commit --date="'+d+'" -m "hack -commit"')
 
-os.system('git push -u origin master')        
+os.system('git push -u origin master')          
